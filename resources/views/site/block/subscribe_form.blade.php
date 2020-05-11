c7401970e820e6be5fa0b85d9d74a9f978271933
venodon <venodon@gmail.com>
2020-05-11 10:48:44 +0300

    subscribe

--- resources/views/site/block/subscribe_form.blade.php
+++ resources/views/site/block/subscribe_form.blade.php
@@ -1,14 +1,14 @@
 <div class="flex-group">
-    <div class="input-holder email-field">
+    <div class="input-holder first_name-field" style="margin-right: 1px;">
         <input type="text" name="first_name" placeholder="Name"/>
     </div>
-    <div class="input-holder email-field">
+    <div class="input-holder last_name-field" style="margin-right: 1px;">
         <input type="text" name="last_name" placeholder="Last name"/>
     </div>
-    <div class="input-holder email-field">
+    <div class="input-holder phone-field" style="margin-right: 1px;">
         <input type="text" name="phone" placeholder="Phone"/>
     </div>
-    <div class="input-holder email-field">
+    <div class="input-holder email-field" style="margin-right: 1px;">
         <input type="email" name="email" placeholder="Email"/>
     </div>
 
