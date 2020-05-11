7ae6da09f0879b509178539ce1de20c0190f40a6
venodon <venodon@gmail.com>
2020-05-11 11:14:04 +0300

    subscribe

--- resources/views/emails/subscription_form.blade.php
+++ resources/views/emails/subscription_form.blade.php
@@ -65,24 +65,6 @@
             {!! empty($email) ? '-' : "<a style=\"color: #332243\">$email</a>" !!}
           </td>
         </tr>
-
-        <tr>
-          <td width="50%"
-            style="font-size: 15px; line-height: 18px; padding: 10px 0; font-family:Verdana, Arial, sans-serif; color:#657482; text-align: left; border-top-width: 1px; border-top-color: #dcbaa1; border-top-style: dashed;">
-            @lang('main.subscribe_to'):
-          </td>
-          <td width="50%"
-            style="font-size: 15px; line-height: 18px; padding: 10px 0; font-family:Verdana, Arial, sans-serif; color:#30343f; text-align: left; border-top-width: 1px; border-top-color: #dcbaa1; border-top-style: dashed;">
-            @if($type_subscribe === 1)
-              @lang('main.objects')
-            @elseif($type_subscribe === 2)
-              @lang('main.blog_articles')
-            @else
-              @lang('main.objects_and_blog_articles')
-            @endif
-          </td>
-        </tr>
-
         <tr>
           <td width="50%" style="border-bottom-width: 1px; border-bottom-color: #dcbaa1; border-bottom-style: dashed;"></td>
           <td width="50%" style="border-bottom-width: 1px; border-bottom-color: #dcbaa1; border-bottom-style: dashed;"></td>
